40eb913bb25ed4b05b52cf4fa377459086dd22b6
avisek123 <sahooavisek41@gmail.com>
2023-04-30 00:24:22 +0530

    folder structure done

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const App = () => {
+const Router = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Router</Text>
     </View>
   );
 };
 
-export default App;
+export default Router;
 
 const styles = StyleSheet.create({});
