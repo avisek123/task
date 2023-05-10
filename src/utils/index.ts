00684fa787c49eabff51559276d8f9750ed00da7
avisek123 <sahooavisek41@gmail.com>
2023-05-10 20:17:32 +0530

    minor chnages done

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export const DATA = [
   {subject: 'Mathematics', time: '9:00 - 10:00', color: 'pink'},
   {subject: 'English Language', time: '10:00 - 11:00', color: 'green'},
   {subject: 'Physics', time: '11:00 - 12:00', color: 'blue'},
+  {subject: 'Computer Science', time: '12:00 - 1:00', color: 'yellow'},
 ];
 export function getRandomColor() {
   const letters = '0123456789ABCDEF';
