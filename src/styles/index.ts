4d5135a8cdffaf2202582761beaf605f31597438
avisek123 <sahooavisek41@gmail.com>
2023-05-10 20:39:10 +0530

    unused code changes done

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,19 +1,5 @@
-import {extendTheme} from 'native-base';
-
 const COLORS = {
   PRIMARY: '#c84e94',
   SECONDARY: '#0A9BDE',
 };
 export default COLORS;
-
-export const CustomTheme = extendTheme({
-  colors: COLORS,
-});
-export const tabBarStyle = {
-  height: 60,
-  position: 'absolute',
-  bottom: 16,
-  right: 16,
-  left: 16,
-  borderRadius: 16,
-};
