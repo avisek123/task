8c95d2a7f178de00dffbcdcbddc4983b17780391
avisek123 <sahooavisek41@gmail.com>
2023-05-14 14:34:53 +0530

    done

--- src/screens/private/Home.tsx
+++ src/screens/private/Home.tsx
@@ -1,15 +1,18 @@
 import {
   ActivityIndicator,
+  BackHandler,
   ListRenderItem,
   RefreshControl,
   StyleSheet,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Box, FlatList, Center} from 'native-base';
 
 import {CampaignCard, Header} from 'components';
 import {useCampaign} from 'hooks';
 import {Campaign} from 'types';
+
+import {Alert} from 'react-native';
 const Home = () => {
   const {data, isLoading, isRefreshing, setIsRefreshing} = useCampaign();
 
@@ -17,7 +20,27 @@ const Home = () => {
     // Render each item here
     return <CampaignCard item={item} />;
   };
-  console.log('data', data);
+  // console.log('data', data);
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert('Confirm Exit', 'Are you sure you want to exit?', [
+        {
+          text: 'Cancel',
+          onPress: () => null,
+          style: 'cancel',
+        },
+        {text: 'YES', onPress: () => BackHandler.exitApp()},
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
   return (
     <Box flex={1} safeArea>
       <Header />
